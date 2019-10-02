2676993dd0490eed6b46385add2e46b82f1d3bee
Walter Ingham <wally18025@outlook.com>
2019-10-02 00:05:47 -0500

    working for time conversions... again

--- HowLong/HowLong.cpp
+++ HowLong/HowLong.cpp
@@ -1,21 +1,30 @@
-// HowLong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Haulong_v1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
 #include <iostream>
+#include "Time.h"
+
+
+using namespace std;
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	/*cout << "\nFirst Date (dd/mm/yyyy): ";
+	UserInput::date1_input();
+	
+	cout << "\nSecond Date (dd/mm/yyyy): ";
+	UserInput::date2_input();*/
+	
+	cout << "\nFirst Time (hh:mm:ss): ";
+	Time t1;
+	t1.time_input();
+
+
+	Time tc;
+	tc.time_calculation();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ HowLong/Time.h
@@ -0,0 +1,110 @@
+﻿#ifndef TIME_H
+#define TIME_H
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+	
+	static char col;
+
+		int hours1=0, minutes1=0, seconds1=0;
+		int hours2=0, minutes2=0, seconds2=0;
+	class Time
+	{
+
+
+	private:
+		//int hour, minute, second;
+		
+	public:
+		
+		void setTime(int hours1, int minutes1, int seconds1);
+		void printTimeOne();
+		void printTimeTwo();	
+
+		static void time_input();
+		static void time_calculation();
+	};
+
+	
+
+
+	void Time::time_input()
+	{
+		cin >> hours1 >> col >> minutes1 >> col >> seconds1;
+		Time pt1;
+		pt1.printTimeOne();
+		cin >> hours2 >> col >> minutes2 >> col >> seconds2;
+		Time pt2;
+		pt2.printTimeTwo();
+	}
+
+inline void Time::setTime(int hours1, int minutes1, int seconds1)
+{
+}
+
+inline void Time::printTimeOne()
+	{
+		cout << setw(2) << setfill('0') << hours1 << ":"
+			<< setw(2) << setfill('0') << minutes1 << ":"
+			<< setw(2) << setfill('0') << seconds1 << "\n";
+	}
+	void Time::printTimeTwo()
+	{
+		cout << setw(2) << setfill('0') << hours2 << ":"
+			<< setw(2) << setfill('0') << minutes2 << ":"
+			<< setw(2) << setfill('0') << seconds2 << "\n";
+	}
+void Time::time_calculation()
+	{
+
+		if (hours1 > hours2 || (hours1 == hours2 && minutes1 > minutes2) || (hours1 == hours2 && minutes1 ==
+			minutes2 && seconds1 > seconds2))
+		{
+			if (seconds1 < seconds2)
+			{
+				seconds1 = seconds1 + 60;
+				minutes1 = minutes1 - 1;
+			}
+			if (minutes1 < minutes2)
+			{
+				minutes1 = minutes1 + 60;
+				hours1 = hours1 - 1;
+			}
+			if (hours1 < hours2)
+			{
+				hours1 = hours1 + 24;
+			}
+
+			static int h_output = (hours1 - hours2);
+			static int m_output = (minutes1 - minutes2);
+			static int s_output = (seconds1 - seconds2);
+
+			cout << "\nPast: " << h_output << "h:" << m_output << "m:" << s_output << "s ago";
+			cin.clear();
+		}
+		else
+		{
+			if (seconds1 > seconds2)
+			{
+				seconds2 = seconds2 + 60;
+				minutes2 = minutes2 - 1;
+			}
+			if (minutes1 > minutes2)
+			{
+				minutes2 = minutes2 + 60;
+				hours2 = hours2 - 1;
+			}
+			if (hours1 > hours2)
+			{
+				hours2 = hours2 + 24;
+			}
+			int h_output = (hours2 - hours1);
+			int m_output = (minutes2 - minutes1);
+			int s_output = (seconds2 - seconds1);
+			(cout << "\nFuture: " << h_output << "h:" << m_output << "m:" << s_output << "s from now");
+			cin.clear();
+		}
+	}
+
+#endif
